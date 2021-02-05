afbbef74d429b9c9b53955fa8766827b1bb11eff
Yuri Ng <yuringuyen312@gmail.com>
2021-02-05 18:39:59 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
 # Add your variables here
+first_number = 4
+
+second_number = 3
+p second_number
+
+sum = first_number + second_number
+
+
+difference = first_number - second_number
+
+
+product = first_number * second_number
+
+quotient = first_number / second_number
