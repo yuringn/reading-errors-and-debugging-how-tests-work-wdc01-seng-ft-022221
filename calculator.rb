243bd14366548e91f43740624459d009b6718ba7
Yuri Ng <yuringuyen312@gmail.com>
2021-02-05 18:37:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,10 @@
 # Add your variables here
-first_number = 2.to_i
-p first_number
+first_number = 2
 
 second_number = 3
 p second_number
 
 sum = first_number + second_number
 p sum
+
+subtracting = first_number - second_number
